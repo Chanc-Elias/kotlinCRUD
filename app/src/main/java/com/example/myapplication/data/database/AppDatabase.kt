d3d8e55687cc67691f636feb33a5771ec7cd05f5
CHANCE ELIAS <Chanceelias9@gmail.com>
2026-02-10 20:48:46 +0300

    Update AppDatabase.kt

--- app/src/main/java/com/example/myapplication/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/myapplication/data/database/AppDatabase.kt
@@ -1,5 +1,5 @@
 package com.example.studentcrudapp.data.database
-   // importing libra
+   // importing libraries
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
